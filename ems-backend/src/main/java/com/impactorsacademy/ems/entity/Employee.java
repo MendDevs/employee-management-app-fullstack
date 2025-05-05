f9c2f575fee588b5115ad55332367d8029a2ae7a
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-05-06 00:46:58 +0200

    entity edited.

--- ems-backend/src/main/java/com/impactorsacademy/ems/entity/Employee.java
+++ ems-backend/src/main/java/com/impactorsacademy/ems/entity/Employee.java
@@ -1,5 +1,26 @@
 package com.impactorsacademy.ems.entity;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_id", unique = true, nullable = false)
+    private String email;
 
 }
