e5703c06bba756f151ce0f6a7939ec4611e18e56
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-05-06 20:28:38 +0200

    updated employee dto

---
+++ ems-backend/src/main/java/com/impactorsacademy/ems/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.impactorsacademy.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository <Employee, Long> {
+    // Custom query methods can be defined here if needed
+    // For example, findByEmail(String email) to find an employee by their email
+
+
+}
