fdcb8354827a9f2e006166d1bd483e81f0f2506b
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-05-08 15:05:03 +0200

    added user to database using 'GET' - Postman

--- ems-backend/src/main/java/com/impactorsacademy/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/impactorsacademy/ems/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.impactorsacademy.ems.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,15 @@ import com.impactorsacademy.ems.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor 
+@AllArgsConstructor //helps us avoid writing the consturctor manually
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/api/employees/")
 public class EmployeeController {
 
     private EmployeeService employeeService;
 
     //Build add Employee REST API
+    @PostMapping 
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto createdEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(createdEmployee, HttpStatus.CREATED);
