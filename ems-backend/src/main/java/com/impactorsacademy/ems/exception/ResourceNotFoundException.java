daf62d9cbe3a604609a744cbca5a2a519a4beeb1
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-05-09 01:50:13 +0200

    progress

---
+++ ems-backend/src/main/java/com/impactorsacademy/ems/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.impactorsacademy.ems.exception;
+
+
+@
+public class ResourceNotFoundException {
+
+}
--- ems-backend/src/main/java/com/impactorsacademy/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/com/impactorsacademy/ems/service/EmployeeService.java
@@ -6,5 +6,6 @@ import com.impactorsacademy.ems.dto.EmployeeDto;
 
 public interface EmployeeService {
   EmployeeDto createEmployee(EmployeeDto employeeDto);
+  EmployeeDto getEmployeeById(Long employeeId);
 }
 
