af2cd70fda2ef2f345ff40dc4df3f2a7c2ed9166
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-05-29 11:35:42 +0200

    frontend

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
 import React, { useState } from 'react';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+      <HeaderComponent/>
+      <Routes>
+        // http://localhost:3000 
+        <Route path='/' element = { <ListEmployeeComponent/>}></Route>
+        //http:localhost:3000/employees
+        <Route path='/employees' element = {<ListEmployeeComponent />}></Route>
+      </Routes>
       <ListEmployeeComponent />
+      <FooterComponent/>
+    </BrowserRouter>
     </>
   )
 }
