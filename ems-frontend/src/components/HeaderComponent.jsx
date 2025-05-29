907f983534e5cd13c854661bdb95a06075ec42ee
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-05-29 10:35:59 +0200

    frontend

---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,18 @@
+// to get the below structure, type rafc and hit enter.
+
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a class="navbar-brand" href="https://www.impactorsacademy.com">Employee Management System</a>
+            </nav>
+        </header>
+      
+    </div>
+  )
+}
+
+export default HeaderComponent
