eff69ec507dcd01a06d5cb20589dbcd949320892
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-05-29 05:03:20 +0200

    database connection

--- ems-backend/src/main/java/com/impactorsacademy/ems/EmsBackendApplication.java
+++ ems-backend/src/main/java/com/impactorsacademy/ems/EmsBackendApplication.java
@@ -2,6 +2,11 @@ package com.impactorsacademy.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @SpringBootApplication
 public class EmsBackendApplication {
@@ -10,4 +15,17 @@ public class EmsBackendApplication {
 		SpringApplication.run(EmsBackendApplication.class, args);
 	}
 
+	@Bean 
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(@NonNull CorsRegistry registry){
+				registry.addMapping("/api/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+						.allowedHeaders("*");
+			}
+		
+		};
+	}
 }
+
