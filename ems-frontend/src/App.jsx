e369aa7f1a7d81c8eda5fc8c75679f34ac923968
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-06-05 23:23:30 +0200

    Employee Component

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EmployeeComponent from './components/EmployeeComponent';
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
           {/* http://localhost:3000 */}
           <Route path='/' element={<ListEmployeeComponent />} />
           {/* http://localhost:3000/employees */}
-          <Route path='/employees' element={<ListEmployeeComponent />} />
-          <Route path='/addEmplyee'></Route>
+          <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:3000/add-employees */}
+          <Route path='/add-employee' element={<EmployeeComponent/>}></Route>
         </Routes>
         <FooterComponent/>
       </BrowserRouter>
--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const EmployeeComponent = () => {
+
+    const [firstName,setFirstName] = useState['']
+    const [lastName, setLastName]  = useState['']
+    const [email, setEmail]        = useState['']
+
   return (
-    <div>
-        
-      
-    </div>
+        <div className='container'> 
+          <div>
+
+          </div>
+        </div>
   )
 }
 
